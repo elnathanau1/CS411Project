c38ca7f8e864538e6253f64a1b6ddabe7eaa74d0
elnathanau1 <elnathanau1@gmail.com>
2019-02-15 16:24:57 -0600

    init commit

---
+++ django/spotifyapp/urls.py
@@ -0,0 +1,29 @@
+"""spotifyapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from spotifyapp_1.views import dash, connect, login, group
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', login, name = 'login'),
+    url(r'^connect/', connect , name = 'connect'),
+    url(r'^dash/', dash, name = 'dash'),
+    url(r'^group/', group, name = 'group'),
+]
