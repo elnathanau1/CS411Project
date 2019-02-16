890c3826fd4fb1d59578af9709a9a697ffb24ffb
elnathanau1 <elnathanau1@gmail.com>
2019-02-15 21:30:29 -0600

    Added html files

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import webbrowser
+import spotipy
+import spotipy.util as util
+import spotipy.oauth2 as oauth2
+
+CLIENT_ID = 'c7c0e5450e374d8581a809b81ad3cb43'
+CLIENT_SECRET = '9e40af53e60b4e77be9465a1beab1ffd'
+REDIRECT_URI = 'http://127.0.0.1:8000/connecting/'
+CACHE = '.spotipyoauthcache'
+
+SCOPE = 'user-library-read, user-top-read'
+
 # Create your views here.
 def dash(request):
-   text = "<h1>This is the Dashboard page</h1>"
-   return HttpResponse(text)
+    context = {}
+    return render(request, 'dash.html', context)
 
 def connect(request):
-  text = '<button type="button">Click Me!</button>'
-  return HttpResponse(text)
+    context = {}
+    return render(request, 'connect.html', context)
+
+def connecting(request):
+    sp_oauth = oauth2.SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, scope=SCOPE, cache_path=CACHE)
+    auth_url = sp_oauth.get_authorize_url()
+    webbrowser.open(auth_url, new=1, autoraise=True)
+    token_info = sp_oauth.get_cached_token()
+
+    text = "<h1>Connecting...</h1>"
+    return HttpResponse(text)
+
 
 def group(request):
-  text = "<h1>This is the Group page</h1>"
-  return HttpResponse(text)
+    context = {}
+    return render(request, 'group.html', context)
 
 def login(request):
-  text = "<h1>This is the Login page</h1>"
-  return HttpResponse(text)
+    context = {}
+    return render(request, 'login.html', context)
