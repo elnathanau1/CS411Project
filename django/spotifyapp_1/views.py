a3d9b054405cbf6d5595530af1f064530b2eb04f
eau3 <eau3@illinois.edu>
2019-02-16 17:00:55 -0600

    Now saving User profile to database

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from spotifyapp_1.models import User
+
 import webbrowser
 import spotipy
 import spotipy.util as util
@@ -25,8 +27,45 @@ def dash(request):
     # Get current user
     spotify = spotipy.Spotify(auth=access_token)
     current_user = spotify.current_user()
+    display_name = current_user['display_name']
+    spotify_id = current_user['id']
 
-    context = {"Name" : current_user['display_name']}
+    # Pull top artists
+    top_artists_long = spotify.current_user_top_artists(limit=25, time_range='long_term')
+    top_artists_medium = spotify.current_user_top_artists(limit=25, time_range='medium_term')
+    top_artists_short = spotify.current_user_top_artists(limit=25, time_range='short_term')
+
+    # Process
+    artist_set = set()
+    genre_dict = {}
+    for list in [top_artists_long, top_artists_medium, top_artists_short]:
+        for artist in list['items']:
+            artist_id = artist['id']
+            artist_set.add(artist_id)
+
+            for genre in artist['genres']:
+                if genre in genre_dict:
+                    genre_dict[genre] += 1
+                else:
+                    genre_dict[genre] = 1
+
+    # normalize values
+    total_genres = sum(genre_dict.values())
+    for key in genre_dict:
+        genre_dict[key] = genre_dict[key] / total_genres * 100.0
+
+    # Save to database
+    tempUser = User()
+    tempUser.username = "eau3"
+    tempUser.spotify_user = spotify_id
+    tempUser.genres = genre_dict
+    tempUser.save()
+
+    # Set the context for variables in html
+    context = {
+    "display_name" : display_name,
+    "spotify_id" : spotify_id
+    }
     return render(request, 'dash.html', context)
 
 def connect(request):
