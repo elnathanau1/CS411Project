c2b1da74cb8c51f1e524873b3ddf7cb5cb58d134
eau3 <eau3@illinois.edu>
2019-03-05 01:23:15 -0600

    Staging for Heroku

--- django/spotifyapp/settings.py
+++ django/spotifyapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,22 +76,22 @@ WSGI_APPLICATION = 'spotifyapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'PORT': '5432',
-
-        # 'NAME': 'pfumdtdb',
-        # 'USER': 'pfumdtdb',
-        # 'PASSWORD': 'jg9ANW1hsTWbj3Ei4808fEvr3LTP1QCD',
-        # 'HOST': 'baasu.db.elephantsql.com',
-
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'PORT': '5432',
+#
+#         # 'NAME': 'pfumdtdb',
+#         # 'USER': 'pfumdtdb',
+#         # 'PASSWORD': 'jg9ANW1hsTWbj3Ei4808fEvr3LTP1QCD',
+#         # 'HOST': 'baasu.db.elephantsql.com',
+#
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
+#     }
+# }
 
 
 # Password validation
@@ -130,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
