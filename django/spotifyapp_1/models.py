c2ccc104f8bbe86d2ebf36e4e6dfb6a32c9cf702
eau3 <eau3@illinois.edu>
2019-03-06 15:41:04 -0600

    update Songs model

--- django/spotifyapp_1/models.py
+++ django/spotifyapp_1/models.py
@@ -50,7 +50,8 @@ class Suggestion(models.Model):
 class Song(models.Model):
     #https://api.spotify.com/v1/artists/{id}/top-tracks
     song_id = models.CharField(primary_key = True, max_length = 50)
-    artist = models.CharField(max_length = 100)
+    artist_id = models.CharField(max_length = 100)
+    artist_name = models.CharField(max_lengh = 100)
     genre = HStoreField(null=True, blank=True)
     popularity = models.IntegerField()
     name = models.CharField(max_length = 100)
