8f606221794b28a80656c56c882c8c43dca85761
mlin37 <mlin37@illinois.edu>
2019-03-07 17:09:21 -0600

    seeing len of sql call

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -33,94 +33,95 @@ def dash(request):
     current_user = spotify.current_user()
     display_name = current_user['display_name']
     spotify_id = current_user['id']
+    print(len(User.objects.raw('SELECT * FROM users WHERE spotify_id = \'{0}\''.format(spotify_id))))
 
-    if len(User.objects.raw('SELECT * FROM users WHERE spotify_id = \'{0}\''.format(spotify_id))) > 0:
-
-        # Pull top artists
-        top_artists_long = spotify.current_user_top_artists(limit=25, time_range='long_term')
-        top_artists_medium = spotify.current_user_top_artists(limit=25, time_range='medium_term')
-        top_artists_short = spotify.current_user_top_artists(limit=25, time_range='short_term')
-
-        # Process
-        artist_genre = {} #stores genres of artist
-        artist_set = set()
-        genre_dict = {}
-        for list in [top_artists_long, top_artists_medium, top_artists_short]:
-            for artist in list['items']:
-                artist_id = artist['id']
-                artist_set.add(artist_id)
-                artist_genre[artist_id] = []
-                for genre in artist['genres']:
-                    artist_genre[artist_id].append(genre)
-                    if genre in genre_dict:
-                        genre_dict[genre] += 1
-                    else:
-                        genre_dict[genre] = 1
-
-        # normalize values
-        total_genres = sum(genre_dict.values())
-        for key in genre_dict:
-            genre_dict[key] = genre_dict[key] / total_genres * 100.0
-
-        # Save to User to database
-        tempUser = User()
-        tempUser.spotify_id = spotify_id
-        tempUser.name = display_name
-        tempUser.genres = genre_dict
-        tempUser.save()
-
-        # Get the top songs from the User's artists
-        songs = {} # dict maps song_id to song_data (dict)
-        track_ids = [] # list of track ids
-        for artist in artist_set:
-            tracks = spotify.artist_top_tracks(artist)
-            count = 0
-            for track in tracks['tracks']:
-                if count < 5:
-                    if track not in track_ids:
-                        track_ids.append(track['id'])
-                    song_data = {}
-                    song_data['song_id'] = track['id']
-                    song_data['name'] = track['name']
-                    song_data['popularity'] = track['popularity']
-                    song_data['genres'] = artist_genre[artist]
-                    for a in track['artists']:
-                        if a['id'] == artist:
-                            song_data['artist_name'] = a['name']
-                            song_data['artist_id'] = a['id']
-                    songs[track['id']] = song_data
-                count = count + 1
-
-        start = 0
-        features = {}
-        while start < len(track_ids):
-            if len(track_ids) - start < 50:
-                af = spotify.audio_features(tracks=track_ids[start : len(track_ids)])
-            else:
-                af = spotify.audio_features(tracks=track_ids[start : start+50])
-            for track in af:
-               feat_data = {}
-               feat_data['mode'] = track['mode']
-               feat_data['acousticness'] = track['acousticness']
-               feat_data['danceability'] = track['danceability']
-               feat_data['energy'] = track['energy']
-               features[track['id']] = feat_data
-            start += 50
-
-        # Use this to fill database
-        for id in songs:
-            tempSong = Song()
-            tempSong.song_id = songs[id]['song_id']
-            tempSong.name = songs[id]['name']
-            tempSong.popularity = songs[id]['popularity']
-            tempSong.genre = songs[id]['genres']
-            tempSong.artist_name = songs[id]['artist_name']
-            tempSong.artist_id = songs[id]['artist_id']
-            tempSong.mode = features[id]['mode']
-            tempSong.acousticness = features[id]['acousticness']
-            tempSong.danceability = features[id]['danceability']
-            tempSong.energy = features[id]['energy']
-            tempSong.save()
+    # if len(User.objects.raw('SELECT * FROM users WHERE spotify_id = \'{0}\''.format(spotify_id))) > 0:
+    #
+    #     # Pull top artists
+    #     top_artists_long = spotify.current_user_top_artists(limit=25, time_range='long_term')
+    #     top_artists_medium = spotify.current_user_top_artists(limit=25, time_range='medium_term')
+    #     top_artists_short = spotify.current_user_top_artists(limit=25, time_range='short_term')
+    #
+    #     # Process
+    #     artist_genre = {} #stores genres of artist
+    #     artist_set = set()
+    #     genre_dict = {}
+    #     for list in [top_artists_long, top_artists_medium, top_artists_short]:
+    #         for artist in list['items']:
+    #             artist_id = artist['id']
+    #             artist_set.add(artist_id)
+    #             artist_genre[artist_id] = []
+    #             for genre in artist['genres']:
+    #                 artist_genre[artist_id].append(genre)
+    #                 if genre in genre_dict:
+    #                     genre_dict[genre] += 1
+    #                 else:
+    #                     genre_dict[genre] = 1
+    #
+    #     # normalize values
+    #     total_genres = sum(genre_dict.values())
+    #     for key in genre_dict:
+    #         genre_dict[key] = genre_dict[key] / total_genres * 100.0
+    #
+    #     # Save to User to database
+    #     tempUser = User()
+    #     tempUser.spotify_id = spotify_id
+    #     tempUser.name = display_name
+    #     tempUser.genres = genre_dict
+    #     tempUser.save()
+    #
+    #     # Get the top songs from the User's artists
+    #     songs = {} # dict maps song_id to song_data (dict)
+    #     track_ids = [] # list of track ids
+    #     for artist in artist_set:
+    #         tracks = spotify.artist_top_tracks(artist)
+    #         count = 0
+    #         for track in tracks['tracks']:
+    #             if count < 5:
+    #                 if track not in track_ids:
+    #                     track_ids.append(track['id'])
+    #                 song_data = {}
+    #                 song_data['song_id'] = track['id']
+    #                 song_data['name'] = track['name']
+    #                 song_data['popularity'] = track['popularity']
+    #                 song_data['genres'] = artist_genre[artist]
+    #                 for a in track['artists']:
+    #                     if a['id'] == artist:
+    #                         song_data['artist_name'] = a['name']
+    #                         song_data['artist_id'] = a['id']
+    #                 songs[track['id']] = song_data
+    #             count = count + 1
+    #
+    #     start = 0
+    #     features = {}
+    #     while start < len(track_ids):
+    #         if len(track_ids) - start < 50:
+    #             af = spotify.audio_features(tracks=track_ids[start : len(track_ids)])
+    #         else:
+    #             af = spotify.audio_features(tracks=track_ids[start : start+50])
+    #         for track in af:
+    #            feat_data = {}
+    #            feat_data['mode'] = track['mode']
+    #            feat_data['acousticness'] = track['acousticness']
+    #            feat_data['danceability'] = track['danceability']
+    #            feat_data['energy'] = track['energy']
+    #            features[track['id']] = feat_data
+    #         start += 50
+    #
+    #     # Use this to fill database
+    #     for id in songs:
+    #         tempSong = Song()
+    #         tempSong.song_id = songs[id]['song_id']
+    #         tempSong.name = songs[id]['name']
+    #         tempSong.popularity = songs[id]['popularity']
+    #         tempSong.genre = songs[id]['genres']
+    #         tempSong.artist_name = songs[id]['artist_name']
+    #         tempSong.artist_id = songs[id]['artist_id']
+    #         tempSong.mode = features[id]['mode']
+    #         tempSong.acousticness = features[id]['acousticness']
+    #         tempSong.danceability = features[id]['danceability']
+    #         tempSong.energy = features[id]['energy']
+    #         tempSong.save()
 
         # print(songs[id]) # data from https://api.spotify.com/v1/artists/{id}/top-tracks
         # print(features[id]) # data from https://api.spotify.com/v1/audio-features
