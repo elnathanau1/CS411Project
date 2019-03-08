58b59e534c0578999056e2121a62979e6b17d301
eau3 <eau3@illinois.edu>
2019-03-07 20:34:46 -0600

    attempt at ajax

---
+++ django/staticfiles/js/spotifyapp_1.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    // AJAX GET
+    $('.get-more').click(function(){
+
+        $.ajax({
+            type: "GET",
+            url: "/ajax/more/",
+            success: function(data) {
+            for(i = 0; i < data.length; i++){
+                $('ul').append('<li>'+data[i]+'</li>');
+            }
+        }
+        });
+
+    });
+});
