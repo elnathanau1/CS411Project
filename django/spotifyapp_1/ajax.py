3888afa4008c870f048435cca411f383101d8ed3
eau3 <eau3@illinois.edu>
2019-03-07 20:45:04 -0600

    debug ajax

--- django/spotifyapp_1/ajax.py
+++ django/spotifyapp_1/ajax.py
@@ -2,8 +2,9 @@ import json
 from django.http import Http404, HttpResponse
 
 def more_todo(request):
+    print("clicked button")
     if request.is_ajax():
-        print("clicked button")
+        print('clicked button 2')
         todo_items = ['Mow Lawn', 'Buy Groceries',]
         data = json.dumps(todo_items)
         return HttpResponse(data, content_type='application/json')
