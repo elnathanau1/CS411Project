67517632399b027043b70c977320c6d313902fcb
eau3 <eau3@illinois.edu>
2019-03-07 20:34:46 -0600

    attempt at ajax

--- django/spotifyapp/urls.py
+++ django/spotifyapp/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from spotifyapp_1.views import dash, connect, login, group, connecting
+from spotifyapp_1.views import *
+from spotifyapp_1.ajax import *
 
 admin.autodiscover()
 
 urlpatterns = [
+    # ajax
+    url(r'^ajax/more/$', ajax.more_todo),
+
+    # pages
     url(r'^admin/', admin.site.urls),
     url(r'^login/', login, name = 'login'),
     url(r'^connect/', connect , name = 'connect'),
