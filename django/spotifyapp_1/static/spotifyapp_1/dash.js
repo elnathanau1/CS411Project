0a47214d7e10e6efc793bf3001dfc70c2475cb4e
eau3 <eau3@illinois.edu>
2019-03-09 01:40:54 -0600

    clean up tutorial code

--- django/spotifyapp_1/static/spotifyapp_1/dash.js
+++ django/spotifyapp_1/static/spotifyapp_1/dash.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+
+  //loaded immediately after page is done loading
   $.ajax({
     type: "GET",
     url: "/ajax/top_artists/",
     success: function(data) {
-
       for(i = 0; i < data.length; i++){
       // jQuery selector
         $('#top_artist_table').append('<tr><th>'+data[i]+'</th></tr>')
