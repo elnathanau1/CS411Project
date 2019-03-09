ac70bfef1874081ebe2ca59e9716173ffdcc1e97
eau3 <eau3@illinois.edu>
2019-03-09 01:15:22 -0600

    change jquery selector

--- django/spotifyapp_1/static/spotifyapp_1/dash.js
+++ django/spotifyapp_1/static/spotifyapp_1/dash.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $.ajax({
-    type: "GET"
-    url: "/ajax/top_artists/"
+    type: "GET",
+    url: "/ajax/top_artists/",
     success: function(data) {
       // jQuery selector
       $('#top_artist_table').append('<tr><th>'+data[i]+'</th></tr>')
     }
-  })
+  });
 
 });
