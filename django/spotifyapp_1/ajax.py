0e5cd4528a6d6e47ace896d3675674203e03371b
eau3 <eau3@illinois.edu>
2019-03-09 00:15:55 -0600

    clean up, remove cache button

--- django/spotifyapp_1/ajax.py
+++ django/spotifyapp_1/ajax.py
@@ -1,10 +1,12 @@
 import json
+import os
 from django.http import Http404, HttpResponse
 
+CACHE = '.spotipyoauthcache'
+
 def more_todo(request):
-    print("clicked button")
     if request.is_ajax():
-        print('clicked button 2')
+        os.remove(CACHE)
         todo_items = ['Mow Lawn', 'Buy Groceries',]
         data = json.dumps(todo_items)
         return HttpResponse(data, content_type='application/json')
