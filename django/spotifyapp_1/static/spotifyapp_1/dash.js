86a24116fa29773538f21864ec05fc359ac8f369
elnathanau1 <elnathanau1@gmail.com>
2019-03-10 00:19:45 -0600

    create group implemented

--- django/spotifyapp_1/static/spotifyapp_1/dash.js
+++ django/spotifyapp_1/static/spotifyapp_1/dash.js
@@ -12,4 +12,50 @@ $(document).ready(function() {
     }
   });
 
+  // AJAX POST
+  $('.group_name').click(function(){
+      $.ajax({
+          type: "POST",
+          url: "/ajax/create_group/",
+          dataType: "json",
+          data: { "new_id": $(".group_id").val(), "new_name": $(".group_name").val() },
+          success: function(data) {
+              alert(data.message);
+          }
+      });
+
+  });
+
+  // CSRF code
+  function getCookie(name) {
+      var cookieValue = null;
+      var i = 0;
+      if (document.cookie && document.cookie !== '') {
+          var cookies = document.cookie.split(';');
+          for (i; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
+  var csrftoken = getCookie('csrftoken');
+
+  function csrfSafeMethod(method) {
+      // these HTTP methods do not require CSRF protection
+      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+      crossDomain: false, // obviates need for sameOrigin test
+      beforeSend: function(xhr, settings) {
+          if (!csrfSafeMethod(settings.type)) {
+              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+          }
+      }
+  });
+
 });
