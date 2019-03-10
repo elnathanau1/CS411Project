75305144c2aa1af8c500d9c8716edf55b4bd4303
eau3 <eau3@illinois.edu>
2019-03-10 00:46:37 -0600

    debug

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -205,7 +205,8 @@ def top_artists_req(request):
         raise Http404
 
 def create_group_req(request):
-    print("create group")
+    print(request.is_ajax())
+    print(request.POST)
     if request.is_ajax() and request.POST:
         new_id = request.POST.get('new_id')
         new_name = request.POST.get('new_name')
