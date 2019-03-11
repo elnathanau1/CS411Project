d8f410dba2308d3d8751aecedb62d065c8f72e0d
melanielin <melanielin97@gmail.com>
2019-03-11 14:37:15 -0500

    removed logout button from connect

--- django/spotifyapp_1/static/spotifyapp_1/connect.js
+++ django/spotifyapp_1/static/spotifyapp_1/connect.js
@@ -1,17 +1,4 @@
 $(document).ready(function() {
-    // AJAX GET
-    $('.log-out').click(function(){
-        $.ajax({
-            type: "GET",
-            url: "/ajax/logout/",
-            success: function(data) {
-            for(i = 0; i < data.length; i++){
-                $('ul').append('<li>'+data[i]+'</li>');
-            }
-        }
-        });
-
-    });
 
     // CSRF code
     function getCookie(name) {
