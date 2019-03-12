3a41b6508d64c68414128122c1ab0aa22d2e970e
elnathanau1 <elnathanau1@gmail.com>
2019-03-12 14:26:45 -0500

    debug table

--- django/spotifyapp_1/static/spotifyapp_1/dash.js
+++ django/spotifyapp_1/static/spotifyapp_1/dash.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
       // jQuery selector
         //$('#list_groups_table').append('<tr><th>'+data.groups[i]+'</th></tr>');
         console.log('<tr><th><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></th></tr>');
-        $('#list_groups_table').append('<tr><th><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></th></tr>')
+        $('#list_groups_table').append('<tr><td><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></td></tr>')
       }
 
     }
