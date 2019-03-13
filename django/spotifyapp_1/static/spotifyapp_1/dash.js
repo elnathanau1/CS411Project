bd9507cb4aa44d7af8daa9a971ad7209def82dd9
elnathanau1 <elnathanau1@gmail.com>
2019-03-12 19:49:56 -0500

    attempt at list of links for groups

--- django/spotifyapp_1/static/spotifyapp_1/dash.js
+++ django/spotifyapp_1/static/spotifyapp_1/dash.js
@@ -20,8 +20,9 @@ $(document).ready(function() {
       for(i = 0; i < data.groups.length; i++){
       // jQuery selector
         //$('#list_groups_table').append('<tr><th>'+data.groups[i]+'</th></tr>');
-        console.log('<tr><th><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></th></tr>');
-        $('#list_groups_table').append('<tr><td><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></td></tr>')
+        // console.log('<tr><th><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></th></tr>');
+        // $('#list_groups_table').append('<tr><td><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></td></tr>')
+          $('#list_groups_table').append('<li><a href=\"https://cs411-spotify.herokuapp.com/group/'+data.ids[i]+'/\"/>'+data.groups[i]+'</a></li>')
       }
 
     }
