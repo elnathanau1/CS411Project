0db78e2e1677b997b84d910f09a58b2d9f027725
Shoham Weiss <shohamw2@illinois.edu>
2019-04-02 22:24:50 -0500

    clean up and checking alert

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $('ul li a').filter(":contains('Login')").remove();
+  $('ul li a').filter(":contains('Connect')").remove();
+  $('ul li a').filter(":contains('Group')").remove();
+  $('ul li a').filter(":contains('Home')").css('background-color', '#fff');
+  $('ul li a').filter(":contains('Home')").css('color', '#000');
+  
   alert($(location).attr('href'));
   if ($(location).attr('href') == 'file:///C:/Users/shoha/Desktop/Web/templates/dash.html') {
     alert('works');
