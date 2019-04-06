77139e59fbb2f3a5e534a251f95cf00f66abe806
elnathanau1 <elnathanau1@gmail.com>
2019-04-06 03:21:49 -0500

    try new format

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -244,19 +244,41 @@ def group_view(request, group_id):
         print(list(G.edges))
         # create display
         # Show with Bokeh
+        # plot = Plot(plot_width=400, plot_height=400,
+        #             x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+        # plot.title.text = "Common Genre Graph"
+        #
+        # node_hover_tool = HoverTool(tooltips=[("node_type", "@node_type"), ("name", "@name")])
+        # plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
+        #
+        # # play around with layouts to see which works best
+        # graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
+        #
+        # graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
+        # graph_renderer.edge_renderer.glyph = MultiLine(line_color="black", line_alpha=0.8, line_width=1)
+        # plot.renderers.append(graph_renderer)
+
         plot = Plot(plot_width=400, plot_height=400,
-                    x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
-        plot.title.text = "Common Genre Graph"
+            x_range=Range1d(-1.1,1.1), y_range=Range1d(-1.1,1.1))
+        plot.title.text = "Graph Interaction Demonstration"
 
-        node_hover_tool = HoverTool(tooltips=[("node_type", "@node_type"), ("name", "@name")])
-        plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
+        plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool())
 
-        # play around with layouts to see which works best
-        graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
+        graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0,0))
 
         graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
-        graph_renderer.edge_renderer.glyph = MultiLine(line_color="black", line_alpha=0.8, line_width=1)
+        graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color=Spectral4[2])
+        graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
+
+        graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=5)
+        graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=5)
+        graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=5)
+
+        graph_renderer.selection_policy = NodesAndLinkedEdges()
+        graph_renderer.inspection_policy = EdgesAndLinkedNodes()
+
         plot.renderers.append(graph_renderer)
+
 
         # save graph
         output_file('django/spotifyapp_1/' + static('spotifyapp_1/test.html')) # SUUUUPER HACKY
