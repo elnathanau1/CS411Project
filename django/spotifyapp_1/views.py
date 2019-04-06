02fcab86f3af10f7a61f896a10c57b2dbf7fe505
elnathanau1 <elnathanau1@gmail.com>
2019-04-06 12:08:01 -0500

    move buttons to under table

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -269,30 +269,6 @@ def group_view(request, group_id):
 
         plot.renderers.append(graph_renderer)
 
-        # option 2
-        # plot = Plot(plot_width=400, plot_height=400,
-        #     x_range=Range1d(-1.1,1.1), y_range=Range1d(-1.1,1.1))
-        # plot.title.text = "Common Genres Graph"
-        #
-        # node_hover_tool = HoverTool(tooltips=[("node_type", "@node_type"), ("name", "@name")])
-        # plot.add_tools(node_hover_tool, TapTool(), BoxSelectTool(), BoxZoomTool(), ResetTool())
-        #
-        # graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0,0))
-        #
-        # graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
-        # graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color=Spectral4[2])
-        # graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
-        #
-        # graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=5)
-        # graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=5)
-        # graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=5)
-        #
-        # graph_renderer.selection_policy = NodesAndLinkedEdges()
-        # graph_renderer.inspection_policy = EdgesAndLinkedNodes()
-
-        # plot.renderers.append(graph_renderer)
-
-
         # save graph
         output_file('django/spotifyapp_1/' + static('spotifyapp_1/test.html')) # SUUUUPER HACKY
         save(plot)
