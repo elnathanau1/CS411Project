0b04a80d4775c59369b702e7c69c0589979672ca
Shoham Weiss <shohamw2@illinois.edu>
2019-04-06 23:44:59 -0500

    trying to fix navbar

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-    if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/dash/') {
-      $('ul li a').filter(":contains('Login')").remove();
-      $('ul li a').filter(":contains('Connect')").remove();
-      $('ul li a').filter(":contains('Group')").remove();
-      $('ul li a').filter(":contains('Home')").css('background-color', '#fff');
-      $('ul li a').filter(":contains('Home')").css('color', '#000');
-    }
+if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/dash/') {
+  $('ul li a').filter(":contains('Login')").remove();
+  $('ul li a').filter(":contains('Connect')").remove();
+  $('ul li a').filter(":contains('Group')").remove();
+  $('ul li a').filter(":contains('Home')").css('background-color', '#fff');
+  $('ul li a').filter(":contains('Home')").css('color', '#000');
+}
 
-    if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/') {
-      $('ul li a').filter(":contains('Log out')").remove();
-      $('ul li a').filter(":contains('Connect')").remove();
-      $('ul li a').filter(":contains('Group')").remove();
-      $('ul li a').filter(":contains('About us')").remove();
-      $('ul li a').filter(":contains('Home')").remove();
-    }
+if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/') {
+  $('ul li a').filter(":contains('Log out')").remove();
+  $('ul li a').filter(":contains('Connect')").remove();
+  $('ul li a').filter(":contains('Group')").remove();
+  $('ul li a').filter(":contains('About us')").remove();
+  $('ul li a').filter(":contains('Home')").remove();
+}
 
-    if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
-      $('ul li a').filter(":contains('Log out')").remove();
-      $('ul li a').filter(":contains('Home')").remove();
-      $('ul li a').filter(":contains('Group')").remove();
-      $('ul li a').filter(":contains('Connect')").remove();
-      $('ul li a').filter(":contains('Login')").remove();
-    }
-});
+if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
+  $('ul li a').filter(":contains('Log out')").remove();
+  $('ul li a').filter(":contains('Home')").remove();
+  $('ul li a').filter(":contains('Group')").remove();
+  $('ul li a').filter(":contains('Connect')").remove();
+  $('ul li a').filter(":contains('Login')").remove();
+}
