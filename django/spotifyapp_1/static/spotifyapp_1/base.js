0cab12925f42f352497f75fb73fab8ac0b7d0716
Shoham Weiss <shohamw2@illinois.edu>
2019-04-06 23:32:11 -0500

    trying to fix navbar

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -7,14 +7,6 @@ $(document).ready(function() {
       $('ul li a').filter(":contains('Home')").css('color', '#000');
     }
 
-    if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/group/{{group_id}}/') {
-      $('ul li a').filter(":contains('Login')").remove();
-      $('ul li a').filter(":contains('Connect')").remove();
-      $('ul li a').filter(":contains('Group')").css('background-color', '#c5c6c7');
-      $('ul li a').filter(":contains('Group')").css('color', 'blue');
-      $('ul li a').filter(":contains('Group')").text('{{group_name}}');
-    }
-
     if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/') {
       $('ul li a').filter(":contains('Log out')").remove();
       $('ul li a').filter(":contains('Connect')").remove();
--- django/spotifyapp_1/static/spotifyapp_1/group_view.js
+++ django/spotifyapp_1/static/spotifyapp_1/group_view.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-    alert($(location).attr('href'));
+  // Reorganizing and styling navigation bar
+  $('ul li a').filter(":contains('Login')").remove();
+  $('ul li a').filter(":contains('Connect')").remove();
+  $('ul li a').filter(":contains('Group')").css('background-color', '#c5c6c7');
+  $('ul li a').filter(":contains('Group')").css('color', 'blue');
+  $('ul li a').filter(":contains('Group')").text('{{group_name}}');
+  
     //loaded immediately after page is done loading
     $.ajax({
         type: "GET",
