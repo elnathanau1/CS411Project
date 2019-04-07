1e97ecfe10146fe865b950a8f46c8a1e74150817
Shoham Weiss <shohamw2@illinois.edu>
2019-04-06 23:40:17 -0500

    trying to fix navbar

--- django/spotifyapp_1/static/spotifyapp_1/group_view.js
+++ django/spotifyapp_1/static/spotifyapp_1/group_view.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
   $('ul li a').filter(":contains('Connect')").remove();
   $('ul li a').filter(":contains('Group')").css('background-color', '#c5c6c7');
   $('ul li a').filter(":contains('Group')").css('color', 'blue');
-  $('ul li a').filter(":contains('Group')").text('{{group_name}}');
-  
+  $('ul li a').filter(":contains('Group')").text(data.group_name);
+
     //loaded immediately after page is done loading
     $.ajax({
         type: "GET",
