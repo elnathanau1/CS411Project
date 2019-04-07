c8b6f7daad1afe91480eeb9c5d8781ebc3be815d
Shoham Weiss <shohamw2@illinois.edu>
2019-04-06 22:25:27 -0500

    trying to fix navbar

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -1,11 +1,37 @@
-$(document).ready(function() {
-  alert($(location).attr('href'));
+$(document).ready(
+  function() {
   if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/dash/') {
-    alert('works');
     $('ul li a').filter(":contains('Login')").remove();
     $('ul li a').filter(":contains('Connect')").remove();
     $('ul li a').filter(":contains('Group')").remove();
     $('ul li a').filter(":contains('Home')").css('background-color', '#fff');
     $('ul li a').filter(":contains('Home')").css('color', '#000');
+  }
+
+  function() {
+  if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/group/') {
+    $('ul li a').filter(":contains('Login')").remove();
+    $('ul li a').filter(":contains('Connect')").remove();
+    $('ul li a').filter(":contains('Group')").css('background-color', '#c5c6c7');
+    $('ul li a').filter(":contains('Group')").css('color', 'blue');
+    $('ul li a').filter(":contains('Group')").text('{{group_name}}');
+  }
+
+  function() {
+  if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/') {
+    $('ul li a').filter(":contains('Log out')").remove();
+    $('ul li a').filter(":contains('Connect')").remove();
+    $('ul li a').filter(":contains('Group')").remove();
+    $('ul li a').filter(":contains('About us')").remove();
+    $('ul li a').filter(":contains('Home')").remove();
+  }
+
+  function() {
+  if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
+    $('ul li a').filter(":contains('Log out')").remove();
+    $('ul li a').filter(":contains('Home')").remove();
+    $('ul li a').filter(":contains('Group')").remove();
+    $('ul li a').filter(":contains('Connect')").remove();
+    $('ul li a').filter(":contains('Login')").remove();
   }
 });
