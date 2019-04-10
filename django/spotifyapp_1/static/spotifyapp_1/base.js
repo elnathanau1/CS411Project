1a54ccc5476655327330d229ff77315bde205f3e
melanielin <melanielin97@gmail.com>
2019-04-10 16:28:39 -0500

    base buttons change color on page

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -3,9 +3,19 @@ if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/dash/') {
   $('ul li a').filter(":contains('Dash')").css('color', '#000');
 }
 
+if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/groups/') {
+    $('ul li a').filter(":contains('Groups')").css('background-color', '#fff');
+    $('ul li a').filter(":contains('Groups')").css('color', '#000');
+}
+
+if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/songs/') {
+    $('ul li a').filter(":contains('Songs')").css('background-color', '#fff');
+    $('ul li a').filter(":contains('Songs')").css('color', '#000');
+}
+
 if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/') {
-  // $('ul li a').filter(":contains('Log out')").remove();
-  $('ul li a').filter(":contains('About us')").remove();
+    $('ul li a').filter(":contains('About us')").css('background-color', '#fff');
+    $('ul li a').filter(":contains('About us')").css('color', '#000');
 }
 
 if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
