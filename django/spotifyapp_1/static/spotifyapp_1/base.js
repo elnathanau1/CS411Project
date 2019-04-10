c50dbd5dc62f8ac1316a91b72f9fae2c7492be86
melanielin <melanielin97@gmail.com>
2019-04-10 16:22:45 -0500

    remove base buttons from login page

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -9,12 +9,17 @@ if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/')
 }
 
 if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
-    $('ul li a').filter(":contains('Log in')").remove();
-    $('ul li a').filter(":contains('Log out')").remove();
     $('ul li a').filter(":contains('Dash')").remove();
+    $('ul li a').filter(":contains('Groups')").remove();
+    $('ul li a').filter(":contains('Songs')").remove();
+    $('ul li a').filter(":contains('About us')").remove();
+    $('ul li a').filter(":contains('Log out')").remove();
 }
 
 if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/connect/') {
-    $('ul li a').filter(":contains('Log out')").remove();
     $('ul li a').filter(":contains('Dash')").remove();
+    $('ul li a').filter(":contains('Groups')").remove();
+    $('ul li a').filter(":contains('Songs')").remove();
+    $('ul li a').filter(":contains('About us')").remove();
+    $('ul li a').filter(":contains('Log out')").remove();
 }
