81586ff80bc848f39d6df172104e1b2b3dd14d15
elnathanau1 <elnathanau1@gmail.com>
2019-04-11 00:18:07 -0500

    remove navbar from login

--- django/spotifyapp_1/static/spotifyapp_1/base.js
+++ django/spotifyapp_1/static/spotifyapp_1/base.js
@@ -1,3 +1,9 @@
+var no_bar = [
+  'https://cs411-spotify.herokuapp.com/login/',
+  'https://cs411-spotify.herokuapp.com/connect/',
+  'https://cs411-spotify.herokuapp.com'
+];
+
 if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/dash/') {
   $('ul li a').filter(":contains('Dash')").css('background-color', '#fff');
   $('ul li a').filter(":contains('Dash')").css('color', '#000');
@@ -18,15 +24,7 @@ if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/about_us/')
     $('ul li a').filter(":contains('About us')").css('color', '#000');
 }
 
-if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/login/') {
-    $('ul li a').filter(":contains('Dash')").remove();
-    $('ul li a').filter(":contains('Groups')").remove();
-    $('ul li a').filter(":contains('Songs')").remove();
-    $('ul li a').filter(":contains('About us')").remove();
-    $('ul li a').filter(":contains('Log out')").remove();
-}
-
-if ($(location).attr('href') == 'https://cs411-spotify.herokuapp.com/connect/') {
+if (no_bar.includes($(location).attr('href'))) {
     $('ul li a').filter(":contains('Dash')").remove();
     $('ul li a').filter(":contains('Groups')").remove();
     $('ul li a').filter(":contains('Songs')").remove();
