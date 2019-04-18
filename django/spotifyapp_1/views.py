211f72531e6f8e37989464a5d7007b3a85a0b527
melanielin <melanielin97@gmail.com>
2019-04-17 23:22:57 -0500

    list suggestions bug

--- django/spotifyapp_1/views.py
+++ django/spotifyapp_1/views.py
@@ -503,7 +503,7 @@ def list_suggestions_req(request):
         group_id = request.session['group_id']
         group = Group.objects.filter(group_id=group_id).first()
         print(group.suggestions)
-        query = Song.objects.filter(song_id__contained_by=list(group.suggestions))
+        query = Song.objects.filter(song_id__in=list(group.suggestions))
 
         for song in query:
             names.append(song.name)
